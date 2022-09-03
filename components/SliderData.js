fd6a10977570b7f75a08efeaba7ff151bbce142b
sashoor <saeedashoor.dev@gmail.com>
2022-09-03 20:24:26 +0100

    added hero and gallery , mobile header compelted

---
+++ components/SliderData.js
@@ -0,0 +1,20 @@
+export const SliderData = [
+
+
+  {
+    image:
+    "https://images.unsplash.com/photo-1636592284266-246c90d8af22?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1542303112-026cbdeec458?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1490723412895-f49eb0a9ce0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1637996434228-c05671f05410?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
+  },
+];
